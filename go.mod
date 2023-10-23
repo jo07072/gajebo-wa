f20ca614a6ad353a874692055bae553b1a56cc8d
= <=>
2023-10-23 22:07:20 +0700

    serious

--- go.mod
+++ go.mod
@@ -5,10 +5,18 @@ go 1.21.2
 require go.mau.fi/whatsmeow v0.0.0-20231018112044-306bf6fef4d2
 
 require (
+	github.com/go-zoox/core-utils v1.2.11 // indirect
+	github.com/go-zoox/fetch v1.7.16 // indirect
+	github.com/go-zoox/headers v1.0.6 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mymmsc/gox v1.3.33 // indirect
+	github.com/tidwall/gjson v1.17.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	rsc.io/qr v0.2.0 // indirect
 )
 
--- handler.go
+++ handler.go
@@ -3,30 +3,42 @@ package main
 import (
 	"context"
 	"fmt"
+	"net"
+	"net/url"
 	"strings"
 
+	"github.com/go-zoox/fetch"
+	"github.com/tidwall/gjson"
 	"go.mau.fi/whatsmeow"
 	"go.mau.fi/whatsmeow/binary/proto"
 	"go.mau.fi/whatsmeow/types/events"
 )
 
 func OnMessage(client *whatsmeow.Client, v *events.Message) {
-	Info("Received a message from %s : %s", v.Info.Chat, v.Message.Conversation)
+	Info("Received a message from %s : %s", v.Info.Chat, v.Message.GetConversation())
 	reply := ""
-	text := v.Message.GetConversation()
+	words := strings.Split(v.Message.GetConversation(), " ")
+	//command := regexp.MustCompile(`\.[a-zA-Z]+`).Split(text)
+	command := words[0]
+	query := words[1:]
 
-	switch text {
+	if command == "" {
+		return
+	}
+
+	switch command {
 	case ".info":
-		reply += "*INFO*\n"
-		reply += ".halo\n"
-		reply += ".cuaca\n"
-		reply += ".quotes\n"
-	case ".halo":
-		reply += fmt.Sprintf("Halo juga, %s", v.Info.PushName)
+		reply += GetInfo()
+	case ".menu":
+		reply += GetInfo()
+	case ".help":
+		reply += GetInfo()
 	case ".cuaca":
-		reply += "Udahlah pasti panas malah pake nanya"
+		reply += GetWeather(strings.Join(query, " "))
 	case ".quotes":
-		reply += "Gak perlu quotes, sana cuci piringnya"
+		reply += GetQuotes()
+	case ".ip":
+		reply += IpLookup(query[0])
 	default:
 		reply = ""
 	}
@@ -36,4 +48,76 @@ func OnMessage(client *whatsmeow.Client, v *events.Message) {
 	if reply != "" {
 		client.SendMessage(context.Background(), v.Info.Chat, &proto.Message{Conversation: &reply})
 	}
+}
+
+func GetInfo() string {
+	return `*- MENU -*
+	
+_.help .menu .info_
+Bantuan untuk menggunakan bot ini
+	
+_.cuaca lokasi_
+Lihat cuaca di lokasi tertentu
+
+_quotes_
+Quotes random tiktok viral 2023
+
+*- HENGKER -*
+_.ip www.site.com_`
+}
+
+func GetWeather(location string) string {
+	format := url.QueryEscape("%l\n\n%C: %t%c\nAngin: %w\nBulan: %m\nTekanan: %P\nIndex UV: %u")
+	response, err := fetch.Get(fmt.Sprintf("https://wttr.in/%s?lang=id&format=%s", location, format))
+
+	if err != nil {
+		Error("Cant get weather : %s", response.Error().Error())
+		return "Gak bisa liat cuaca, coba lagi nanti"
+	}
+
+	return string(response.Body) + "\n\n_~ wttr.in_"
+}
+
+func GetQuotes() string {
+	// 	{
+	// "_id": "VZAbqrJVc59C",
+	// "content": "Friendship with oneself is all important because without it one cannot be friends with anybody else in the world.",
+	// "author": "Eleanor Roosevelt",
+	// "tags": [
+	// "Famous Quotes"
+	// ],
+	// "authorSlug": "eleanor-roosevelt",
+	// "length": 113,
+	// "dateAdded": "2020-12-10",
+	// "dateModified": "2023-04-14"
+	// }
+
+	response, err := fetch.Get(fmt.Sprintf("https://api.quotable.io/random"))
+	json := string(response.Body)
+
+	if err != nil || !gjson.Valid(json) {
+		Error("Cant get quotes : %s", response.Error().Error())
+		return "Gak bisa dapetin quote untukmu, coba lagi nanti"
+	}
+
+	quote := gjson.Get(json, "content").String()
+	author := gjson.Get(json, "author").String()
+
+	s := fmt.Sprintf("```%s```\n\n_~ %s_", quote, author)
+
+	return s
+}
+
+func IpLookup(url string) string {
+	Info("IP Lookup : %s", url)
+
+	s := ""
+	ips, _ := net.LookupIP(url)
+	for _, ip := range ips {
+		if ipv4 := ip.To4(); ipv4 != nil {
+			s += fmt.Sprintf("%s\n", ipv4)
+		}
+	}
+
+	return s
 }
